7f78ef9b13f4522e8f060e0e22c3cc14be8b4cc9
Leonardo Santana <leonardops6@al.insper.edu.br>
2026-05-06 15:37:43 -0300

    Janela inicial

---
+++ main.py
@@ -0,0 +1,29 @@
+# ===== Inicialização =====
+# ----- Importa e inicia pacotes
+import pygame
+
+pygame.init()
+
+# ----- Gera tela principal
+window = pygame.display.set_mode((900, 700))
+pygame.display.set_caption('Donkey Kong Remastered (VERSÃO INSPER)')
+
+# ----- Inicia estruturas de dados
+game = True
+
+# ===== Loop principal =====
+while game:
+    # ----- Trata eventos
+    for event in pygame.event.get():
+        # ----- Verifica consequências
+        if event.type == pygame.QUIT:
+            game = False
+
+    # ----- Gera saídas
+    window.fill((200, 0, 0))  # Preenche com a cor branca
+
+    # ----- Atualiza estado do jogo
+    pygame.display.update()  # Mostra o novo frame para o jogador
+
+# ===== Finalização =====
+pygame.quit()  #
